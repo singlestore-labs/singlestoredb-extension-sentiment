7099633aa8f20737f1c915b9bdce2672fbd1e418
Peter Vetere <pvetere@singlestore.com>
2024-06-04 16:53:24 -0400

    fix table defn

--- load_example_data.sql
+++ load_example_data.sql
@@ -1,20 +1,20 @@
 create table youtube (
-  video_id text not null primary key,
-  title text,
+  video_id text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci not null primary key,
+  title text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   publishedAt datetime,
-  channelId text,
-  channelTitle text,
+  channelId text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  channelTitle text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   categoryId int,
   trending_date datetime,
-  tags text,
+  tags text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   view_count int,
   likes int,
   dislikes int,
   comment_count int,
-  thumbnail_link text,
-  comments_disabled text,
-  ratings_disabled text,
-  video_description text
+  thumbnail_link text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  comments_disabled text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  ratings_disabled text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  video_description text character set utf8mb4 collate utf8mb4_general_ci
 );
 
 
